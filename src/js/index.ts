41e1ccd7f9847610338b23957f77dbc461e69dad
James Morrison <james@frontastic.cloud>
2022-09-13 16:32:55 +0100

    Fixed roken imports

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,13 @@
-import { CartMethods, AccountMethods, Extension, WishlistMethods } from "@commercetools/types/src/js/Extension";
+import { CartMethods, AccountMethods, ComposableCommerce, WishlistMethods } from "./types/Extension";
 import { defaultAccount, defaultCart, defaultWishlist } from "./defaults";
 
-export class CoreSDK implements Extension {
+export class CoreSDK implements ComposableCommerce {
+    sdk: any; //TODO: define and export type in ./types/Extension
     cart: CartMethods = defaultCart;
     account: AccountMethods = defaultAccount;
     wishlist: WishlistMethods = defaultWishlist;
 
-    constructor(extension: Extension) {
+    constructor(extension: ComposableCommerce) {
 
     }
 }
