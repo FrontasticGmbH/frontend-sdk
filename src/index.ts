66df3e20f10812ff0b4d7943da49d223c597a292
James Morrison <james@frontastic.cloud>
2022-10-25 19:02:50 +0100

    Seperate domain types into own package

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Extension } from "./library/Extension";
 import { SDK } from "./library/SDK";
+import { REMEMBER_ME } from "./library/types";
 
 const sdk = new SDK();
 
-export { sdk, SDK, Extension };
+export { sdk, SDK, Extension, REMEMBER_ME };
