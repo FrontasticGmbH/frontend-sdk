215f2f457d2b721158b354af7522f7420478c0bb
James Morrison <james@frontastic.cloud>
2022-11-16 08:43:38 +0000

    Updated condition test for CSR

--- src/helpers/cookieManagement.ts
+++ src/helpers/cookieManagement.ts
@@ -3,17 +3,18 @@ const REMEMBER_ME = "__rememberMe";
 
 export const rememberMeCookie = {
     get: function () {
-        if (window) {
+        if (typeof window !== "undefined") {
             return window.localStorage.getItem(REMEMBER_ME);
         }
+        return null;
     },
     set: function () {
-        if (window) {
+        if (typeof window !== "undefined") {
             window.localStorage.setItem(REMEMBER_ME, "1");
         }
     },
     remove: function () {
-        if (window) {
+        if (typeof window !== "undefined") {
             window.localStorage.removeItem(REMEMBER_ME);
         }
     }
