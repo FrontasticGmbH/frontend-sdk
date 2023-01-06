966de1379341230054b905f168f4928a585b61ce
James Morrison <james@frontastic.cloud>
2023-01-06 09:53:44 +0000

    Exported SDKResponse type from package index

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,15 @@ import { Event } from "./library/Event";
 import { FetchError } from "./library/FetchError";
 import { ActionError } from "./library/ActionError";
 import { PageError } from "./library/PageError";
-
-const sdk = new SDK();
+import { SDKResponse } from "./library/types";
 
 export {
-    sdk,
     SDK,
     Extension,
     Event,
     FetchError,
     ActionError,
     PageError,
+    SDKResponse,
     rememberMeCookie
 };
