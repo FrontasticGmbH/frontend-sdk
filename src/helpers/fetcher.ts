011142818f5af83864d4034d73ee598f1ba72e85
James Morrison <james@frontastic.cloud>
2023-01-18 09:57:03 +0000

    Run linting

--- src/helpers/fetcher.ts
+++ src/helpers/fetcher.ts
@@ -5,56 +5,56 @@ import { FetchError } from "../library/FetchError";
 const cookiesApi = Cookies.withAttributes({ path: "/" });
 
 export const fetcher = async <T>(
-	url: string,
-	// currently string to simplify. fetch
-	// also allows URLLike and Request:
-	// url: RequestInfo,
-	options: RequestInit = {},
+    url: string,
+    // currently string to simplify. fetch
+    // also allows URLLike and Request:
+    // url: RequestInfo,
+    options: RequestInit = {},
 ): Promise<T | FetchError> => {
-	const sessionCookie = cookiesApi.get("frontastic-session");
+    const sessionCookie = cookiesApi.get("frontastic-session");
 
-	// rewrite headers, adding our required default headers
-	options.headers = {
-		"Content-Type": "application/json",
-		Accept: "application/json",
-		// "X-Commercetools-Access-Token": "APIKEY", // TODO: unsupported, needs backend work
-		"X-Frontastic-Access-Token": "APIKEY",
-		...(options.headers || {}),
-		...(sessionCookie ? { "Frontastic-Session": sessionCookie } : {}),
-	};
+    // rewrite headers, adding our required default headers
+    options.headers = {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        // "X-Commercetools-Access-Token": "APIKEY", // TODO: unsupported, needs backend work
+        "X-Frontastic-Access-Token": "APIKEY",
+        ...(options.headers || {}),
+        ...(sessionCookie ? { "Frontastic-Session": sessionCookie } : {}),
+    };
 
-	const response: Response = await fetch(url, options);
+    const response: Response = await fetch(url, options);
 
-	if (
-		typeof window !== "undefined" &&
-		response.ok &&
-		response.headers.has("Frontastic-Session")
-	) {
-		let rememberMe = rememberMeCookie.get();
-		let expiryDate;
+    if (
+        typeof window !== "undefined" &&
+        response.ok &&
+        response.headers.has("Frontastic-Session")
+    ) {
+        let rememberMe = rememberMeCookie.get();
+        let expiryDate;
 
-		if (rememberMe) {
-			expiryDate = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 3); // 3 months
-		}
+        if (rememberMe) {
+            expiryDate = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 3); // 3 months
+        }
 
-		cookiesApi.set(
-			"frontastic-session",
-			response.headers.get("Frontastic-Session")!,
-			{ expires: expiryDate },
-		);
-	}
+        cookiesApi.set(
+            "frontastic-session",
+            response.headers.get("Frontastic-Session")!,
+            { expires: expiryDate },
+        );
+    }
 
-	if (response.ok) {
-		return response.json();
-	}
+    if (response.ok) {
+        return response.json();
+    }
 
-	let error: Error | string;
+    let error: Error | string;
 
-	try {
-		error = await response.clone().json();
-	} catch (e) {
-		error = await response.text();
-	}
+    try {
+        error = await response.clone().json();
+    } catch (e) {
+        error = await response.text();
+    }
 
-	return new FetchError(error);
+    return new FetchError(error);
 };
--- src/library/Event.ts
+++ src/library/Event.ts
@@ -1,31 +1,31 @@
 export class Event<
-	EventName extends string = string,
-	EventData extends any = void,
+    EventName extends string = string,
+    EventData extends any = void,
 > {
-	public eventName: EventName;
-	public data: EventData;
-	public isDefaultPrevented: boolean;
-	public isCancelled: boolean;
-	public isPropagationStopped: boolean;
+    public eventName: EventName;
+    public data: EventData;
+    public isDefaultPrevented: boolean;
+    public isCancelled: boolean;
+    public isPropagationStopped: boolean;
 
-	constructor(options: { eventName: EventName; data: EventData }) {
-		this.eventName = options.eventName;
-		this.data = options.data;
+    constructor(options: { eventName: EventName; data: EventData }) {
+        this.eventName = options.eventName;
+        this.data = options.data;
 
-		this.isCancelled = false;
-		this.isDefaultPrevented = false;
-		this.isPropagationStopped = false;
-	}
+        this.isCancelled = false;
+        this.isDefaultPrevented = false;
+        this.isPropagationStopped = false;
+    }
 
-	public preventDefault(): void {
-		this.isDefaultPrevented = true;
-	}
+    public preventDefault(): void {
+        this.isDefaultPrevented = true;
+    }
 
-	public cancel(): void {
-		this.isCancelled = true;
-	}
+    public cancel(): void {
+        this.isCancelled = true;
+    }
 
-	public stopPropagation(): void {
-		this.isPropagationStopped = true;
-	}
+    public stopPropagation(): void {
+        this.isPropagationStopped = true;
+    }
 }
