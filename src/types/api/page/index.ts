53ea25c49be077e191dcc2dd9faef66b08b6ec99
James Morrison <james@frontastic.cloud>
2023-03-17 16:33:04 +0000

    Added getPreview page API method definition

--- src/types/api/page/index.ts
+++ src/types/api/page/index.ts
@@ -1,15 +1,13 @@
 import { SDKResponse } from "../../../library/types";
+import { PagePreviewResponse } from "./PagePreviewResponse";
 import { PageResponse } from "./PageResponse";
 import { PageViewData } from "./PageViewData";
 
 type PageApi = {
-	getPage: (options: {
-		path: string
-	}) => Promise<SDKResponse<PageResponse>>
-}
+	getPage: (options: { path: string }) => Promise<SDKResponse<PageResponse>>;
+	getPreview: (options: {
+		previewId: string;
+	}) => Promise<SDKResponse<PagePreviewResponse>>;
+};
 
-export {
-	PageApi,
-	PageResponse,
-	PageViewData
-}
+export { PageApi, PageResponse, PagePreviewResponse, PageViewData };
